2600a97004bf55dba7aa3a4bbe99cc42d74b614e
abhilash15500 <89824888+abhilash15500@users.noreply.github.com>
2024-09-06 11:07:14 +0530

    Remove the menu heading

--- src/menu.js
+++ src/menu.js
@@ -106,11 +106,8 @@ function onMenuClick() {
     menuRightContainer.textContent = "";
     menuLeftContainer.textContent = "";
 
-    const menuHeading = document.createElement("h1");
-    menuHeading.classList.add("menu-heading");
-    menuHeading.textContent = "Xplozion's Cafe Menu";
-
-    contentDiv.appendChild(menuHeading);
+    
+  
 
     appendCoffeeInfo();
     appendPizzaInfo();
